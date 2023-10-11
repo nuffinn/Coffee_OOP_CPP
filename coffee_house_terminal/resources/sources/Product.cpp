419da0178e27edde677a62280b24690aa5cb7226
StarBurst <shodaighoul@gmail.com>
2023-10-11 16:27:27 +0700

    create source file for product

---
+++ coffee_house_terminal/resources/sources/Product.cpp
@@ -0,0 +1,29 @@
+#include "Product.h"
+
+Product::Product(const std::string& name, double price, int productID)
+    : name(name), price(price), productID(productID) {}
+
+std::string Product::GetName() const {
+    return name;
+}
+
+double Product::GetPrice() const {
+    return price;
+}
+
+int Product::GetProductID() const {
+    return productID;
+}
+
+void Product::SetName(const std::string& newName) {
+    name = newName;
+}
+
+void Product::SetPrice(double newPrice) {
+    price = newPrice;
+}
+
+void Product::SetProductID(int newProductID) {
+    productID = newProductID;
+}
+
