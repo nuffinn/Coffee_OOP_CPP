6957f1d5c4577e4aa38991de926a4776a8f113c3
StarBurst <shodaighoul@gmail.com>
2023-10-11 16:26:06 +0700

    create header file for product

---
+++ coffee_house_terminal/resources/headers/Product.h
@@ -0,0 +1,24 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <string>
+
+class Product {
+private:
+    std::string name;
+    double price;
+    int productID;
+
+public:
+    Product(const std::string& name, double price, int productID);
+
+    std::string GetName() const;
+    double GetPrice() const;
+    int GetProductID() const;
+    void SetName(const std::string& newName);
+    void SetPrice(double newPrice);
+    void SetProductID(int newProductID);
+};
+
+#endif
+
