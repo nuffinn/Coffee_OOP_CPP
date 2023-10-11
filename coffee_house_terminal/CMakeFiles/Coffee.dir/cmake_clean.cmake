d988fa687603b3e42a09912faeb7cf764b5edc4f
StarBurst <shodaighoul@gmail.com>
2023-10-11 16:23:49 +0700

    initial commit

---
+++ coffee_house_terminal/CMakeFiles/Coffee.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coffee.dir/main.cpp.o"
+  "CMakeFiles/Coffee.dir/main.cpp.o.d"
+  "Coffee"
+  "Coffee.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coffee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
